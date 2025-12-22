b7c7e8c3a52bbe891dd2c1ceadd7d9456498f2b1
sarthakgupta124 <sarthakgupta9259@gmail.com>
2025-12-22 12:22:32 +0530

    Creating project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-10 text-3xl font-bold">
+      This is a new project
+    </div>
+    </>
+  )
+}
+
+export default App
